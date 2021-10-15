bd68cc0c9d72c5072bc8a2614ea4121045f4f2b0
Jor02 <Jor02games@gmail.com>
2021-10-15 20:28:01 +0200

    Renamed GamePatch to LoaderPatch

--- GOILevelImporter/Core/Patches/LoaderPatch.cs
+++ GOILevelImporter/Core/Patches/LoaderPatch.cs
@@ -8,7 +8,7 @@ using UnityEngine.SceneManagement;
 namespace GOILevelImporter.Core.Patches
 {
     [HarmonyPatch(typeof(Loader), "ContinueGame")]
-    class GamePatch
+    class LoaderPatch
     {
         static void Prefix(Loader __instance, ref bool ___loadFinished, ref bool ___safeToClick)
         {
